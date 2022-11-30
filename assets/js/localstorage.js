63accf4d4c8a2f662e1d21c160da799e6d8cdf7d
EMA1970 <114694158+EMA1970@users.noreply.github.com>
2022-11-30 13:44:11 -0800

    edit

--- assets/js/localstorage.js
+++ assets/js/localstorage.js
@@ -20,6 +20,6 @@ window.localStorage.setItem('potatoes', JSON.stringify(ingreidnets));
 function updateFave() {
     var values = [], keys = Object.keys(localstorage), i = keys.length;
     while (i--) { values.push( localStorage.getItem(keys[i]) ); }
-    document.getElementById("fave-currently").textContent = values;
+    document.getElementById("personal-fave").textContent = values;
 }
 
